62d297bc0e719e4259baca06cbc23c102ee710cc
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-01-17 12:13:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,10 @@
     function showPage() {
     document.getElementById("loader-wrapper").style.opacity = "0";
     document.getElementById("myDiv").style.opacity = "1";
+    document.querySelector("body").style.margin = "0 0 0 0";
 }
     function display(){
     document.getElementById("loader-wrapper").style.display = "none";
+    document.querySelector("body").style.margin = "0 0 0 0";
 
 }
