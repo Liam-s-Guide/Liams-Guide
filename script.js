016b28a1478839a1f4c17bc7410b020421ecd21c
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-01-23 21:32:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
     let myVar;
 
     function myFunction() {
-    myVar = setTimeout(showContent, 6000);
-    myVar = setTimeout(showPage, 6500);
-    myVar = setTimeout(display, 8050);
+    myVar = setTimeout(showContent, 5500);
+    myVar = setTimeout(showPage, 6000);
+    myVar = setTimeout(display, 7550);
+    myVar = setTimeout(liamSlide, 6250);
 }
     function showContent(){
     document.getElementById("myDiv").style.display = "block";
@@ -18,3 +19,6 @@
     document.querySelector("body").style.margin = "0 0 0 0";
 
 }
+    function liamSlide(){
+        document.getElementById("liam").style.opacity = "1";
+    }
