a1ea252074f7f2c6f8f682cd4ad30d5871823986
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-01-26 22:57:51 +0100

    Add files via upload

--- guide/script.js
+++ guide/script.js
@@ -23,3 +23,29 @@
     function liamSlide(){
         document.getElementById("liam").style.opacity = "1";
     }
+
+    function hamburger(){
+        myVar = setTimeout(opacityOn, 100);
+        document.getElementById("myDiv").style.display = "none";
+        document.getElementById("myDiv").style.opacity = "0";
+        document.getElementById("hamburgerMenu").style.display = "block";
+        function opacityOn(){
+            document.getElementById("hamburgerMenu").style.opacity = "1";
+        }
+
+    }
+
+    function hamburgerClose(){
+        myVarGuide = setTimeout(opacityOff, 400);
+        myVarGuide = setTimeout(opacityOff2, 450);
+        document.getElementById("hamburgerMenu").style.opacity = "0";
+        function opacityOff(){
+            document.getElementById("hamburgerMenu").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        }
+        function opacityOff2(){
+            document.getElementById("myDiv").style.opacity = "1";
+        }
+
+
+    }
